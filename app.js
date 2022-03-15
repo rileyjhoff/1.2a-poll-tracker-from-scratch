3df7bf5d26a8efd859a95c124e8a86f680fe9df4
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-15 16:56:08 -0700

    made a new app.js file to fix tab indentation issue

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+    // get user input
+    // use user input to update state
+    // update DOM to reflect the new state
